5935a12ae6f4ccb87c956350030b614ed3780199
Priyapta <priaptanaufal@gmail.com>
2025-05-27 09:51:15 +0700

    fix ui

--- src/components/reviews/review-actions.tsx
+++ src/components/reviews/review-actions.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+/* eslint-disable-line @typescript-eslint/no-explicit-any */
 export default function ReviewActions({ review }: { review: any }) {
-  /* eslint-disable-line @typescript-eslint/no-explicit-any */
   const token =
     typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
